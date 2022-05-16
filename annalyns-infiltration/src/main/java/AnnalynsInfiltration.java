44e8da221e27fa481f7975785bf7e713ffc095c3
pt888 <49671598+pt888@users.noreply.github.com>
2022-05-16 12:25:03 +0530

    Add files via upload

---
+++ annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,21 @@
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+      return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        if(knightIsAwake || archerIsAwake || prisonerIsAwake) return true;
+        return false;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+          if(prisonerIsAwake && !archerIsAwake) return true;
+        return false;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+      if(petDogIsPresent && !archerIsAwake) return true;
+        if(prisonerIsAwake && !knightIsAwake && !archerIsAwake) return true;
+        return false;
+    }
+}
